91b08eddefa53118cf83561f9a09e224b3168f81
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-03-01 19:49:32 +0900

    chore: changelog.config.jsを作成

---
+++ changelog.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  disableEmoji: true,
+  format: '{type}{scope}: {emoji}{subject}',
+  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues'],
+  scopes: ['', 'deps', 'docs', 'pages'],
+};
