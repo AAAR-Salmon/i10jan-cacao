7f4c516bee1df80932156cbb52142c608783f56a
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-03-02 02:08:58 +0900

    feat: cacaoのAPIトークンを設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CACAO_API_TOKEN: process.env.CACAO_API_TOKEN
+  },
 };
 
 module.exports = nextConfig;
