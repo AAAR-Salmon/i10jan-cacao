defa05f445b9f94b2d7f6e386e497a48baf16347
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-03-04 12:04:23 +0900

    refactor(pages): フレーバーテキストを返す関数を切り分けて可読性向上

--- pages/someone.tsx
+++ pages/someone.tsx
@@ -8,6 +8,20 @@ type Props = {
   countInRoom: number;
 };
 
+const flavorText = (ok: boolean, n: number) => {
+  if (!ok) return '悲しい';
+  if (n < 0) {
+    const decoder = new TextDecoder('utf-8');
+    const data = [
+      0xbbef, 0xe8bf, 0xa38b, 0x8eee, 0xeb84, 0x8182, 0x87e8, 0xeea3, 0xaa8e, 0x92e8, 0xeb81, 0xafb3, 0xaae0, 0x009f,
+    ];
+    return decoder.decode(Uint16Array.from(data).buffer);
+  }
+  if (n === 0) return 'あなたが1番乗り？';
+  if (n < 10) return '来て来て 部室';
+  return 'ちょっと多すぎるかもよ';
+};
+
 const SomeonePage: NextPage<Props> = (props) => {
   return (
     <>
@@ -18,22 +32,7 @@ const SomeonePage: NextPage<Props> = (props) => {
       </Head>
       <Container fluid='md' className='d-flex flex-column justify-content-center'>
         <p className='text-center fs-2'>{props.successFetch ? `${props.countInRoom}人いる！` : '通信失敗…'}</p>
-        <p className='text-center'>
-          {((ok: boolean, n: number) => {
-            if (!ok) return '悲しい';
-            if (n < 0) {
-              const decoder = new TextDecoder('utf-8');
-              const data = [
-                0xbbef, 0xe8bf, 0xa38b, 0x8eee, 0xeb84, 0x8182, 0x87e8, 0xeea3, 0xaa8e, 0x92e8, 0xeb81, 0xafb3, 0xaae0,
-                0x009f,
-              ];
-              return decoder.decode(Uint16Array.from(data).buffer);
-            }
-            if (n === 0) return 'あなたが1番乗り？';
-            if (n < 10) return '来て来て 部室';
-            return 'ちょっと多すぎるかもよ';
-          })(props.successFetch, props.countInRoom)}
-        </p>
+        <p className='text-center'>{flavorText(props.successFetch, props.countInRoom)}</p>
       </Container>
     </>
   );
