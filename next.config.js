733ef5e21d1a8625b2bd59a5bb17a9a2422bb5e9
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-03-06 12:46:30 +0900

    fix: next.config.jsからenvを削除
    Next v9以前で必要だったが今は必要ないらしい

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    CACAO_API_TOKEN: process.env.CACAO_API_TOKEN
-  },
 };
 
 module.exports = nextConfig;
